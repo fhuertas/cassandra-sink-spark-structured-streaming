4a801f691d4c373596738662fd7eb1706e8e4ef8
Francisco Huertas <francisco@fhuertas.com>
2018-03-22 12:47:40 +0100

    Connector now support Spark 2.2.1 (#6)
    * Prepare release
    
    Version in other file
    Sleep for init db change to 30
    remove unused files for previos tests
    
    * Add support for Spark 2.2.1
    
    Remove the use of holden karau testing library because compilation with spark 2.2.1 had problems

--- build.sbt
+++ build.sbt
@@ -1,6 +1,5 @@
 scalaVersion := "2.11.12"
 crossScalaVersions := Seq("2.11.12", "2.10.7")
-version := "1.0.0-SNAPSHOT"
-name := "cassandra-sink_2.2.0"
+name := "cassandra-sink_2.2.1"
 organization := "com.fhuertas"
 libraryDependencies ++= Dependencies.testDependencies
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,14 +6,15 @@ object Dependencies {
   // Versions
   lazy val scalaTestVersion = "3.0.4"
   lazy val typesafeConfigVersion = "1.3.2"
+  lazy val scalaCheckVersion = "1.13.5"
 
   //Spark and cassandra dependencies are set in library.sbt with runtime script
 
   // Testing libraries
   val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion
   val typesafeConfig = "com.typesafe" % "config" % typesafeConfigVersion
-
+  val scalaCheck = "org.scalacheck" %% "scalacheck" % scalaCheckVersion
   val testDependencies: Seq[ModuleID] =
-    Seq(scalaTest, typesafeConfig).map(_ % "test")
+    Seq(scalaTest, typesafeConfig, scalaCheck).map(_ % "test")
 
 }
